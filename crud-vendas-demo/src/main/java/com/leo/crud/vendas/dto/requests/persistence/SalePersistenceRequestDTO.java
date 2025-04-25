b6ba153a41061c811ea3abefb91cfe6a7ad047d0
Leonardo Henrique <leohencosta18@gmail.com>
2025-04-24 20:00:29 -0400

    added validations in some dtos and modified some dtos

--- crud-vendas-demo/src/main/java/com/leo/crud/vendas/dto/requests/persistence/SalePersistenceRequestDTO.java
+++ crud-vendas-demo/src/main/java/com/leo/crud/vendas/dto/requests/persistence/SalePersistenceRequestDTO.java
@@ -1,8 +1,33 @@
 package com.leo.crud.vendas.dto.requests.persistence;
 
+import com.leo.crud.vendas.validations.constraints.DateNotExceeding;
+import com.leo.crud.vendas.validations.constraints.NotRetroactiveDate;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
-public record SalePersistenceRequestDTO(Date dateSale, String bankId, BigDecimal totalPrice, String paymentMethod, Integer amountProducts, List<ProductPersistenceRequestDTO> products) {
+public record SalePersistenceRequestDTO(
+
+        @DateNotExceeding(message = "A data não pode ser superior a data atual")
+        @NotRetroactiveDate(message = "A data não pode ser retroativa em relação a data atual")
+        Date dateSale,
+
+        @NotBlank(message = "Campo bankId não pode ser nulo")
+        String bankId,
+
+        @NotNull(message = "O campo preço total não pode ser nulo")
+        BigDecimal totalPrice,
+
+        @NotBlank(message = "O campo metodo de pagamento não pode ser nulo")
+        Long paymentMethod,
+
+        @NotNull(message = "O campo quantidade de produtos não pode ser nulo")
+        Integer amountProducts,
+
+        @NotBlank(message = "A lista de produtos não pode ser vazia")
+        List<ProductPersistenceRequestDTO> products
+) {
 }
