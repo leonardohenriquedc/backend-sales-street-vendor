bdd5d506aadbf3472d39a0d46d8a299bdf3c08d3
Leonardo Henrique <leohencosta18@gmail.com>
2025-05-02 20:57:06 -0400

    added dto to authenticator

--- crud-vendas-demo/src/main/java/com/leo/crud/vendas/dto/requests/persistence/UserAuthRequestDTO.java
+++ crud-vendas-demo/src/main/java/com/leo/crud/vendas/dto/requests/persistence/UserAuthRequestDTO.java
@@ -3,7 +3,7 @@ package com.leo.crud.vendas.dto.requests.persistence;
 import jakarta.validation.constraints.NotBlank;
 
 public record UserAuthRequestDTO(
-        @NotBlank  String email,
-        @NotBlank String password
+   @NotBlank String email,
+   @NotBlank String password
 ) {
 }
