7e778f0331b9db2eb67cfd6b3b5ac16340eaa61f
Leonardo Henrique <leohencosta18@gmail.com>
2025-05-19 17:43:10 -0400

    added new query

--- crud-vendas-demo/src/main/java/com/leo/crud/vendas/repositories/ProductAmountRepository.java
+++ crud-vendas-demo/src/main/java/com/leo/crud/vendas/repositories/ProductAmountRepository.java
@@ -3,6 +3,7 @@ package com.leo.crud.vendas.repositories;
 import com.leo.crud.vendas.entities.ProductAmount;
 import com.leo.crud.vendas.entities.ProductAmountKey;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,4 +15,11 @@ public interface ProductAmountRepository extends JpaRepository<ProductAmount, Pr
     Optional<ProductAmount> findBySaleId(Long id);
 
     List<ProductAmount> findAllBySaleId(Long id);
+
+    @Query(nativeQuery = true, value = "select pa.* " +
+            "from tb_products_amounts pa " +
+            "inner join tb_sales " +
+            "on tb_sales.id = pa.sale_id " +
+            "where tb_sales.external_id= :externalId ;")
+    List<ProductAmount> findAllBySaleExternalId(String externalId);
 }
